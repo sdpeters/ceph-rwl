4409ba8b608f673311d44c1d05071b34da2637cb
Yehuda Sadeh <yehuda@inktank.com>
2013-02-21 14:56:11 -0800

    common/ceph_parser.cc: cleanup
    remove extra logging to stdout
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/common/ceph_json.cc
+++ src/common/ceph_json.cc
@@ -56,7 +56,6 @@ JSONObj::~JSONObj()
 
 void JSONObj::add_child(string el, JSONObj *obj)
 {
-  cout << "add_child: " << name << " <- " << el << std::endl;
   children.insert(pair<string, JSONObj *>(el, obj));
 }
 
@@ -170,7 +169,6 @@ JSONObj *JSONObj::get_parent()
 
 bool JSONObj::is_object()
 {
-  cout << data.type() << std::endl;
   return (data.type() == obj_type);
 }
 
