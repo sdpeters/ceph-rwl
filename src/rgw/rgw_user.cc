590b4d2e8ca12df07969e26dd43cd9dd188367db
Yehuda Sadeh <yehuda@inktank.com>
2013-02-28 16:48:57 -0800

    rgw: rgw_get_system_obj() can return obj_version
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_user.cc
+++ src/rgw/rgw_user.cc
@@ -125,7 +125,7 @@ int rgw_get_user_info_from_index(RGWRados *store, string& key, rgw_bucket& bucke
   bufferlist bl;
   RGWUID uid;
 
-  int ret = rgw_get_obj(store, NULL, bucket, key, bl);
+  int ret = rgw_get_system_obj(store, NULL, bucket, key, bl, NULL);
   if (ret < 0)
     return ret;
 
@@ -150,7 +150,7 @@ int rgw_get_user_info_by_uid(RGWRados *store, string& uid, RGWUserInfo& info)
   bufferlist bl;
   RGWUID user_id;
 
-  int ret = rgw_get_obj(store, NULL, store->zone.user_uid_pool, uid, bl);
+  int ret = rgw_get_system_obj(store, NULL, store->zone.user_uid_pool, uid, bl, NULL);
   if (ret < 0)
     return ret;
 
