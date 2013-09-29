04284295747158be078c60f809aca38d2b170cd7
Noah Watkins <noahwatkins@gmail.com>
2013-09-29 09:04:37 -0700

    compat: enable lseek64 alias
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/include/compat.h
+++ src/include/compat.h
@@ -13,8 +13,11 @@
 #define CEPH_COMPAT_H
 
 #if defined(__FreeBSD__)
-#define	lseek64(fd, offset, whence)	lseek(fd, offset, whence)
 #define	ENODATA	61
+#endif /* !__FreeBSD__ */
+
+#if defined(__FreeBSD__) || defined(__APPLE__)
+#define	lseek64(fd, offset, whence)	lseek(fd, offset, whence)
 #endif /* !__FreeBSD__ */
 
 #ifndef TEMP_FAILURE_RETRY
