xinxinsh/ceph

inttypes: detect and define missing integer types
Working around missing integer types is pretty easy. For example, the
__u32 family are Linux-specific types, and using these in Ceph
internally is fine because we can typedef them, but the biggest problem
is that they are also used in installed headers. Again, they could be
typedefed in the installed headers, but it isn't clear that we can't
just use standard ints in installed headers.

Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

src/crush/mapper.c
@@ -16,6 +16,7 @@
 # define dprintk(args...) /* printf(args) */
 # define kmalloc(x, f) malloc(x)
 # define kfree(x) free(x)
+# include "include/inttypes.h"
 #endif
 
 #include "crush.h"

src/include/linux_fiemap.h
@@ -18,6 +18,8 @@
 #include "include/inttypes.h"
 #endif
 
+#include "include/inttypes.h"
+
 struct fiemap_extent {
 	__u64 fe_logical;  /* logical offset in bytes for the start of
 			    * the extent from the beginning of the file */

sdpeters/ceph-rwl

Conflicts:
	src/include/byteorder.h

src/crush/mapper.c
@@ -16,6 +16,7 @@
 # define dprintk(args...) /* printf(args) */
 # define kmalloc(x, f) malloc(x)
 # define kfree(x) free(x)
+# include "include/int_types.h"
 #endif
 
 #include "crush.h"

src/include/linux_fiemap.h
@@ -18,6 +18,8 @@
 #include <sys/types.h>
 #endif
 
+#include "include/int_types.h"
+
 struct fiemap_extent {
 	__u64 fe_logical;  /* logical offset in bytes for the start of
 			    * the extent from the beginning of the file */