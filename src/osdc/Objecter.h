6827470f02f20a24f1e9101c323aec604b62b213
Yehuda Sadeh <yehuda@inktank.com>
2014-03-13 14:36:26 -0700

    objecter: get rid of osdmap_lock
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/osdc/Objecter.h
+++ src/osdc/Objecter.h
@@ -1032,7 +1032,6 @@ private:
   version_t last_seen_pgmap_version;
 
   RWLock rwlock;
-  RWLock osdmap_lock;
   Mutex timer_lock;
   SafeTimer timer;
 
@@ -1546,7 +1545,6 @@ public:
     last_seen_osdmap_version(0),
     last_seen_pgmap_version(0),
     rwlock("Objecter::rwlock"),
-    osdmap_lock("Objecter::osdmap_lock"),
     timer_lock("Objecter::timer_lock"),
     timer(cct, timer_lock),
     logger(NULL), tick_event(NULL),
