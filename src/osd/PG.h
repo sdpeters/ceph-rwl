23ee2c3622e236ada5696508ebb6489a1ddca8a1
Sage Weil <sage@inktank.com>
2014-05-17 19:36:40 -0700

    osd/PG: uninline PG::init_primary_up_acting()
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -2009,39 +2009,7 @@ public:
     const vector<int> &newup,
     const vector<int> &newacting,
     int new_up_primary,
-    int new_acting_primary) {
-    actingset.clear();
-    acting = newacting;
-    for (uint8_t i = 0; i < acting.size(); ++i) {
-      if (acting[i] != CRUSH_ITEM_NONE)
-	actingset.insert(
-	  pg_shard_t(
-	    acting[i],
-	    pool.info.ec_pool() ? shard_id_t(i) : shard_id_t::NO_SHARD));
-    }
-    up = newup;
-    if (!pool.info.ec_pool()) {
-      up_primary = pg_shard_t(new_up_primary, shard_id_t::NO_SHARD);
-      primary = pg_shard_t(new_acting_primary, shard_id_t::NO_SHARD);
-      return;
-    }
-    up_primary = pg_shard_t();
-    primary = pg_shard_t();
-    for (uint8_t i = 0; i < up.size(); ++i) {
-      if (up[i] == new_up_primary) {
-	up_primary = pg_shard_t(up[i], shard_id_t(i));
-	break;
-      }
-    }
-    for (uint8_t i = 0; i < acting.size(); ++i) {
-      if (acting[i] == new_acting_primary) {
-	primary = pg_shard_t(acting[i], shard_id_t(i));
-	break;
-      }
-    }
-    assert(up_primary.osd == new_up_primary);
-    assert(primary.osd == new_acting_primary);
-  }
+    int new_acting_primary);
   pg_shard_t get_primary() const { return primary; }
   
   int        get_role() const { return role; }
