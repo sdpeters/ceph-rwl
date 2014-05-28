99bc8975c67900f1faaaf149f335436056a3eded
Yehuda Sadeh <yehuda@inktank.com>
2014-05-28 12:12:31 -0700

    objecter: don't serialize responses if there's no object name
    This implicitly fixes an issue with the list_objects() being reentrant,
    and triggers a lock dependency issue. The better solution would be to
    have the callback context specify whether it's reentrant or not, but
    will require a much bigger change.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/osdc/Objecter.cc
+++ src/osdc/Objecter.cc
@@ -2327,7 +2327,7 @@ void Objecter::handle_osd_op_reply(MOSDOpReply *m)
   }
 
   /* get it before we call _finish_op() */
-  Mutex *completion_lock = s->get_lock(op->target.base_oid);
+  Mutex *completion_lock = (op->target.base_oid.name.size() ? s->get_lock(op->target.base_oid) : NULL);
 
   // done with this tid?
   if (!op->onack && !op->oncommit) {
@@ -2338,8 +2338,9 @@ void Objecter::handle_osd_op_reply(MOSDOpReply *m)
   ldout(cct, 5) << num_unacked.read() << " unacked, " << num_uncommitted.read() << " uncommitted" << dendl;
 
   // serialize completions
-
-  completion_lock->Lock();
+  if (completion_lock) {
+    completion_lock->Lock();
+  }
   s->lock.unlock();
 
   // do callbacks
@@ -2349,7 +2350,9 @@ void Objecter::handle_osd_op_reply(MOSDOpReply *m)
   if (oncommit) {
     oncommit->complete(rc);
   }
-  completion_lock->Unlock();
+  if (completion_lock) {
+    completion_lock->Unlock();
+  }
 
   m->put();
   s->put();
