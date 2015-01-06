6f10708edb259718f2b066cedea154b280d0aa7f
Sage Weil <sage@redhat.com>
2015-01-06 11:20:24 -0800

    osd: change coll_t::is_pg() and is_temp() to take a pointer; drop snap
    The snap arg isn't used anymore.  The spg_t should be a pointer (not ref)
    since it is an output argument.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/tools/ceph_objectstore_tool.cc
+++ src/tools/ceph_objectstore_tool.cc
@@ -455,9 +455,7 @@ int _action_on_all_objects(ObjectStore *store, action_on_object_t &action, bool 
   for (vector<coll_t>::iterator i = candidates.begin();
        i != candidates.end();
        ++i) {
-    spg_t pgid;
-    snapid_t snap;
-    if (i->is_pg(pgid, snap)) {
+    if (i->is_pg()) {
       colls_to_check.push_back(*i);
     }
   }
@@ -693,7 +691,7 @@ void dump_log(Formatter *formatter, ostream &out, pg_log_t &log,
 void remove_coll(ObjectStore *store, const coll_t &coll)
 {
   spg_t pg;
-  coll.is_pg_prefix(pg);
+  coll.is_pg_prefix(&pg);
   OSDriver driver(
     store,
     coll_t(),
@@ -752,11 +750,10 @@ int finish_remove_pgs(ObjectStore *store)
        it != ls.end();
        ++it) {
     spg_t pgid;
-    snapid_t snap;
 
-    if (it->is_temp(pgid) ||
+    if (it->is_temp(&pgid) ||
 	it->is_removal(&pgid) ||
-	(it->is_pg(pgid, snap) && PG::_has_removal_flag(store, pgid))) {
+	(it->is_pg(&pgid) && PG::_has_removal_flag(store, pgid))) {
       cout << "finish_remove_pgs " << *it << " removing " << pgid << std::endl;
       OSD::recursive_remove_collection(store, pgid, *it);
       continue;
@@ -1533,7 +1530,7 @@ int get_object(ObjectStore *store, coll_t coll, bufferlist &bl, OSDMap &curmap)
     coll_t(),
     OSD::make_snapmapper_oid());
   spg_t pg;
-  coll.is_pg_prefix(pg);
+  coll.is_pg_prefix(&pg);
   SnapMapper mapper(&driver, 0, 0, 0, pg.shard);
 
   assert(g_ceph_context);
@@ -1544,8 +1541,7 @@ int get_object(ObjectStore *store, coll_t coll, bufferlist &bl, OSDMap &curmap)
     pg_t pgid = curmap.raw_pg_to_pg(raw_pgid);
   
     spg_t coll_pgid;
-    snapid_t coll_snap;
-    if (coll.is_pg(coll_pgid, coll_snap) == false) {
+    if (coll.is_pg(&coll_pgid) == false) {
       cerr << "INTERNAL ERROR: Bad collection during import" << std::endl;
       return -EFAULT;
     }
@@ -2082,7 +2078,7 @@ int do_list(ObjectStore *store, string pgidstr, string object, Formatter *format
 int do_remove_object(ObjectStore *store, coll_t coll, ghobject_t &ghobj)
 {
   spg_t pg;
-  coll.is_pg_prefix(pg);
+  coll.is_pg_prefix(&pg);
   OSDriver driver(
     store,
     coll_t(),
@@ -3104,24 +3100,17 @@ int main(int argc, char **argv)
 
   // Find pg
   for (it = ls.begin(); it != ls.end(); ++it) {
-    snapid_t snap;
     spg_t tmppgid;
 
-    if (!it->is_pg(tmppgid, snap)) {
+    if (!it->is_pg(&tmppgid)) {
       continue;
     }
 
-    if (it->is_temp(tmppgid)) {
+    if (it->is_temp(&tmppgid)) {
       continue;
     }
 
     if (op != "list-pgs" && tmppgid != pgid) {
-      continue;
-    }
-    if (snap != CEPH_NOSNAP) {
-      if (debug)
-        cerr << "skipping snapped dir " << *it
-	       << " (pg " << pgid << " snap " << snap << ")" << std::endl;
       continue;
     }
 
