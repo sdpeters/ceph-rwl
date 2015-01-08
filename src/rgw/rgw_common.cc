87256b4d26bf4ee97224d086005e932a26f4377c
Yehuda Sadeh <yehuda@redhat.com>
2015-01-08 11:46:15 -0800

    rgw: accept 'versions subresource
    Fixes: #10482
    Backport: giant, firefly
    We want bucket listing with versions param to succeed and not to fail on
    signing.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -528,6 +528,7 @@ int XMLArgs::parse()
           (name.compare("partNumber") == 0) ||
           (name.compare("uploadId") == 0) ||
           (name.compare("versionId") == 0) ||
+          (name.compare("versions") == 0) ||
           (name.compare("torrent") == 0)) {
         sub_resources[name] = val;
       } else if (name[0] == 'r') { // root of all evil
