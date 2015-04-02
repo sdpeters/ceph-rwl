be5c9c098bcb2b5694c9b549b3e1654669a8c60e
Yehuda Sadeh <yehuda@redhat.com>
2015-04-01 17:22:04 -0700

    rgw: add no-precondition-error http param for object delete
    In case we don't want to get an error response when a precondition
    failed.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -2177,6 +2177,9 @@ void RGWDeleteObj::execute()
       delete_marker = del_op.result.delete_marker;
       version_id = del_op.result.version_id;
     }
+    if (ret == -ERR_PRECONDITION_FAILED && no_precondition_error) {
+      ret = 0;
+    }
   } else {
     ret = -EINVAL;
   }
