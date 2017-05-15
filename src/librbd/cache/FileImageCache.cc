zhouyuan/ceph

librbd: adding write thru mode support for rbd ssd cache
Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

src/librbd/cache/FileImageCache.cc
@@ -587,8 +587,14 @@ struct C_WriteBlockRequest : BlockGuard::C_BlockRequest {
       req = new C_WriteToImageRequest<I>(cct, image_writeback,
                                          std::move(block_io), bl, req);
     } else {
-      // block is now dirty -- can't be replaced until flushed
-      policy.set_dirty(block_io.block);
+      if (0 == policy.get_write_mode()) {
+        //write-thru
+        req = new C_WriteToImageRequest<I>(cct, image_writeback,
+                                           std::move(block_io), bl, req);
+      } else {
+        // block is now dirty -- can't be replaced until flushed
+        policy.set_dirty(block_io.block);
+      }
       req = new C_WriteToMetaRequest<I>(cct, meta_store, block_io.block, &policy, req);
 
       if (block_io.partial_block) {
@@ -800,6 +806,9 @@ FileImageCache<I>::FileImageCache(ImageCtx &image_ctx)
     m_release_block(std::bind(&FileImageCache<I>::release_block, this,
                               std::placeholders::_1)),
     m_lock("librbd::cache::FileImageCache::m_lock") {
+  CephContext *cct = m_image_ctx.cct;
+  uint8_t write_mode = cct->_conf->rbd_persistent_cache_enabled?1:0;
+  m_policy->set_write_mode(write_mode);
 }
 
 template <typename I>

sdpeters/ceph-rwl

src/librbd/cache/FileImageCache.cc
@@ -587,8 +587,14 @@ struct C_WriteBlockRequest : BlockGuard::C_BlockRequest {
       req = new C_WriteToImageRequest<I>(cct, image_writeback,
                                          std::move(block_io), bl, req);
     } else {
-      // block is now dirty -- can't be replaced until flushed
-      policy.set_dirty(block_io.block);
+      if (0 == policy.get_write_mode()) {
+        //write-thru
+        req = new C_WriteToImageRequest<I>(cct, image_writeback,
+                                           std::move(block_io), bl, req);
+      } else {
+        // block is now dirty -- can't be replaced until flushed
+        policy.set_dirty(block_io.block);
+      }
       req = new C_WriteToMetaRequest<I>(cct, meta_store, block_io.block, &policy, req);
 
       IOType io_type = static_cast<IOType>(block_io.io_type);
@@ -610,7 +616,7 @@ struct C_WriteBlockRequest : BlockGuard::C_BlockRequest {
         // TODO optimize by only reading missing extents
         promote_buffers.emplace_back();
 
-        if (block_io.tid > 0) {
+        if (block_io.tid > 0 && (1 == policy.get_write_mode())) {
           req = new C_AppendEventToJournal<I>(cct, journal_store, block_io.tid,
                                               block_io.block, IO_TYPE_WRITE,
                                               req);
@@ -638,7 +644,7 @@ struct C_WriteBlockRequest : BlockGuard::C_BlockRequest {
         }
       } else {
         // full block overwrite
-        if (block_io.tid > 0) {
+        if (block_io.tid > 0 && (1 == policy.get_write_mode())) {
           req = new C_AppendEventToJournal<I>(cct, journal_store, block_io.tid,
                                               block_io.block, IO_TYPE_WRITE,
                                               req);
@@ -806,6 +812,9 @@ FileImageCache<I>::FileImageCache(ImageCtx &image_ctx)
     m_release_block(std::bind(&FileImageCache<I>::release_block, this,
                               std::placeholders::_1)),
     m_lock("librbd::cache::FileImageCache::m_lock") {
+  CephContext *cct = m_image_ctx.cct;
+  uint8_t write_mode = cct->_conf->rbd_persistent_cache_writeback?1:0;
+  m_policy->set_write_mode(write_mode);
 }
 
 template <typename I>